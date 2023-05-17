0e8d93dd73be11566d187a7f472d11617cc377b0
Taha-SOUMBATE <soumbate.taha@gmail.com>
2023-05-17 17:12:03 +0100

    lo

--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -10,35 +10,33 @@ const Page = () => {
     return (
         <div className='w-[70vw]  mx-auto font-bold'>
 
-            <section>
-                <div className="lg:flex justify-between lg:flex-row md:flex  md:flex-row text-xl">
-                    <div className='w-[34vw] text-start '>
-                        <h2>Accélérez votre carrière,formez-vous aux<br /></h2>
-                        <h2>métiers de la Tech</h2>
-                        <p className='text-base'>Accédez à de nouvelles opportunités grâce à nos formations immersives en développement web et en data.</p>
-                        <button className='rounded-md bg-orange-400 p-1 text-white'>no formation</button>
-
-                    </div>
-                    <div id="carouselExampleRide" class="carousel slide w-[15vw]" data-bs-ride="true">
-                        <div class="carousel-inner ">
-                            {photo.map((photos, index) => (
-                                <div key={photos.id} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
-                                    <img src={photos.pick} className="d-block " alt="" />
-                                </div>
-                            ))}
-
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
-                </div>
-            </section>
+<section className="my-5">
+  <div className="justify-between md:flex md:flex-row text-xl">
+    <div className="w-full md:w-1/2 lg:w-1/3 text-start">
+      <h2>Accélérez votre carrière, formez-vous aux<br /></h2>
+      <h2 className="text-orange-400 hover:text-orange-100 bg-orange-100 hover:bg-orange-400 w-1/4 md:w-3/4 p-1 rounded-lg">métiers de la Tech</h2>
+      <p className="text-base">Accédez à de nouvelles opportunités grâce à nos formations immersives en développement web et en data.</p>
+      <a className="rounded-md no-underline bg-orange-400 p-1 text-white">no formation</a>
+    </div>
+    <div id="carouselExampleRide" className="carousel slide w-1/4 md:w-1/2 lg:w-1/3 me-4" data-bs-ride="true">
+      <div className="carousel-inner">
+        {photo.map((photos, index) => (
+          <div key={photos.id} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+            <img src={photos.pick} className="d-block w-full" alt="" />
+          </div>
+        ))}
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+</section>
             <section>
                 <h2 className='text-center'>Partenaires recruteurs</h2>
                 <div id="carouselExampleAutoplaying" className="carousel slide flex justify-center" data-bs-ride="carousel">
