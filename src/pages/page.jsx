535db3c79033e20654ec8318baaaa92649853676
Taha-SOUMBATE <soumbate.taha@gmail.com>
2023-05-17 20:21:37 +0100

    kuyf

--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -68,41 +68,37 @@ const Page = () => {
                 <p className='text-lg md:w-[80vw] lg:w-[55vw] mx-auto  text-center   p-1 rounded-lg'>Dès la fin de la formation, notre équipe Carrières vous accompagnera dans votre recherche d'emploi pour trouver le job de vos rêves.</p>
             </section>
 
-            <section className='hidden'>
-                <h2>Nos bootcamps immersifs en Web </h2>
+            <section className=''>
+                <h2 className='text-2xl text-orange-400 hover:text-orange-100 bg-orange-100 hover:bg-orange-400 md:w-[80vw] lg:w-[80vw] mx-auto  text-center   p-1 rounded-lg'>Nos bootcamps immersifs en Web </h2>
                 <p>Des programmes qui ont aidé des milliers d'étudiants à se lancer dans de nouvelles carrières dans l'industrie tech.</p>
 
-                <div className="row">
+                <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-2 gap-4">
 
                     {
                         card.map((cards) => (
-
-                            <div className="col-3 mb-3">
-                                <div class="card " key={cards.id}>
-                                    <img src={cards.img} class="card-img-top" alt="..." />
-                                    <div class="card-body h-52">
-                                        <h5 class="card-title">{cards.title}</h5>
-                                        <p class="card-text">{cards.dsck}</p>
-                                        <a href="#" class="btn btn-primary">{cards.btne}</a>
-                                    </div>
+                            <div class="card " key={cards.id}>
+                                <img src={cards.img} class="card-img-top" alt="..." />
+                                <div class="card-body lg:h-[20vh] md:h-[25vh] flex flex-col justify-between ">
+                                    <h5 class="card-title text-lg">{cards.title}</h5>
+                                    <p class="card-text text-sm">{cards.dsck}</p>
+                                    <a href="#" class="btn btn-primary">{cards.btne}</a>
                                 </div>
                             </div>
-
                         ))
                     }
                 </div>
 
 
             </section>
-            <section className='hidden'>
-                <h2>Pourquoi notre méthodologie est efficace ?</h2>
-                <div className="row ">
+            <section className=''>
+                <h2 className='text-2xl text-orange-400 hover:text-orange-100 bg-orange-100 hover:bg-orange-400 md:w-[80vw] lg:w-[78vw] mx-auto  text-center   p-1 rounded-lg'>Pourquoi notre méthodologie est efficace ?</h2>
+                <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4 ">
                     {
                         parcour.map((parcours) => (
-                            <div className="col-3  mb-3">
-                                <div class="card" key={parcours.id}>
-                                    <div class="card-body h-64">
-                                        <h5 class="card-title text-center ">{parcours.nom}</h5>
+                            <div className="  mb-3">
+                                <div class="card " key={parcours.id}>
+                                    <div class="card-body md:h-[33vh] lg:h-[28vh] h-[20vh] flex flex-col justify-between">
+                                        <h5 class="card-title md:text-lg lg:text-lg">{parcours.nom}</h5>
                                         <p class="card-text">Durée {parcours.time}</p>
 
 
@@ -113,18 +109,18 @@ const Page = () => {
                     }
                 </div>
             </section>
-            <section className='hidden'>
-                <h2 className='text-2xl'>Des formations diplômantes faites par des experts plus que passionnés
+            <section className=''>
+                <h2 className='lg:text-2xl md:text-lg text-base  text-orange-400 hover:text-orange-100 bg-orange-100 hover:bg-orange-400 md:w-[80vw] lg:w-[78vw] mx-auto  text-center   p-1 rounded-lg'>Des formations diplômantes faites par des experts plus que passionnés
                     pour vous mener au succès professionnel.</h2>
-                <div className="row">
+                <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-2 gap-4">
 
                     {
                         dipliom.map((diplioms) => (
 
-                            <div className="col-4 mb-3">
+                            <div className=" mb-3">
                                 <div class="card " key={diplioms.id}>
-                                    <img src={diplioms.img} class="card-img-top" alt="..." />
-                                    <div class="card-body h-28 ">
+                                    <img src={diplioms.img} class="card-img-top h-[20vh]" alt="..." />
+                                    <div class="card-body  lg:h-[15vh] md:h-[16vh]">
                                         <h4 class="card-title text-xl font-bold">{diplioms.titel}</h4>
                                         <p class="card-text">{diplioms.text}</p>
                                     </div>
