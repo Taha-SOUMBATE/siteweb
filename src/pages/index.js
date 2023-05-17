e0834c7e148b329755ab45ea757aeee80bc4338f
Taha-SOUMBATE <soumbate.taha@gmail.com>
2023-05-17 14:45:59 +0100

    nice

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,17 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navbare from '@/components/Navbare'
 import Page from './page'
+import Footer from '@/components/Footer'
 
 
 
 export default function Home() {
   return (
     <>
-    
+
     <Navbare/>
     <Page/>
+    <Footer/>
     </>
   )
 }
