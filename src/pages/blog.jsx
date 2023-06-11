d1d37a232647dc6544045265a7eac254be843e4f
Taha-SOUMBATE <soumbate.taha@gmail.com>
2023-06-11 13:25:09 +0100

    ytf

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,10 +1,22 @@
 import Navbare from '@/components/Navbare';
+import Image from 'next/image';
 import React from 'react';
-
+import image from '../pictures/campus-habous.jpeg'
 const Blog = () => {
     return (
         <div>
             <Navbare />
+            <section className='section'>
+                <h2 className='titel'>Au-delà d'un Simple Programme</h2>
+                <div className="grid grid-cols-3 gap-4">
+                    <div className="relative">
+                        <span  className='img' >
+                        <Image src={image} height={500} width={500} alt="img" />
+
+                        </span>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
